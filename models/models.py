494dac9dd428f6e059d587ffb4d5109009e6a051
dachi beruchashvili <beruchashvili.dachi@kiu.edu.ge>
2025-04-12 18:39:13 +0400

    add Book instance creation from csv row | add Book instance creation from json object

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 import re
 
+import bs4.element
 from bs4 import BeautifulSoup
 
 
@@ -9,27 +10,30 @@ class Book:
         "BOOK_COVER_CLASS": "book-cover"
     }
 
-    def __init__(self, node):
-        self.__data = {}
+    def __init__(self, node=None, data=None):
+        if data is None:
+            data = {}
+        self.__data = data
         self.__construct_book(node)
 
-    def __construct_book(self, node: BeautifulSoup):
-        try:
-            cover = node.find("div", class_=self.__constants["BOOK_COVER_CLASS"])
-            link = cover.find("a")
-            meta = cover.find("img")
-            self.__data["title"] = meta["title"]
-            author = re.search(".+ by (.+)", meta["title"])
-            if author:
-                author = author.group(1)
-            self.__data["author"] = author
-            cover = re.search("(^//covers.+)", meta["src"])
-            if cover:
-                cover = "https:" + cover.group(1)
-            self.__data["cover"] = cover
-            self.__data["link"] = self.__constants["BASE_URL"] + link["href"]
-        except TypeError or KeyError:
-            print(node)
+    def __construct_book(self, node):
+        if node:
+            try:
+                cover = node.find("div", class_=self.__constants["BOOK_COVER_CLASS"])
+                link = cover.find("a")
+                meta = cover.find("img")
+                self.__data["title"] = meta["title"]
+                author = re.search(".+ by (.+)", meta["title"])
+                if author:
+                    author = author.group(1)
+                self.__data["author"] = author
+                cover = re.search("(^//covers.+)", meta["src"])
+                if cover:
+                    cover = "https:" + cover.group(1)
+                self.__data["cover"] = cover
+                self.__data["link"] = self.__constants["BASE_URL"] + link["href"]
+            except TypeError or KeyError or AttributeError:
+                self.__data = {}
 
     def get(self, key):
         return self.__data.get(key)
@@ -41,5 +45,16 @@ class Book:
         return self.__data
 
     @staticmethod
+    def from_list(l):
+        data = {}
+        for i, key in enumerate(Book.keys()):
+            data[key] = l[i] or None
+        return Book(data=data)
+
+    @staticmethod
+    def from_dict(d):
+        return Book(data=d)
+
+    @staticmethod
     def keys():
-        return ["title", "author", "category", "cover", "link"]
+        return ["title", "author", "cover", "link"]
