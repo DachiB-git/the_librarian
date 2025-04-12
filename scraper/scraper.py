87118c8e4158b08f586b654e99c47613fc76c83c
dachi beruchashvili <beruchashvili.dachi@kiu.edu.ge>
2025-04-12 17:58:25 +0400

    add save as json file | add save as csv file

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,20 +1,27 @@
+import csv
+import json
 import re
 
 import requests
 from bs4 import BeautifulSoup
 from utils.parser import JSON
+from models.models import Book
 
 
 class Librarian:
     __constants = {
         "BASE_URL": "https://openlibrary.org",
         "CAROUSEL_CLASS": "carousel-section",
-        "CAROUSEL_HEADER_CLASS": "carousel-section-header"
+        "CAROUSEL_HEADER_CLASS": "carousel-section-header",
+        "CAROUSEL_CONTAINER_CLASS": "carousel-container",
+        "CAROUSEL_ITEM_CLASS": "book",
     }
 
     def __get_home_page_content(self):
         try:
             res = requests.get(self.__constants["BASE_URL"], verify=True)
+            if not res.ok:
+                raise requests.exceptions.RequestException(f"Error: {res.status_code}. Please try again later.")
             return BeautifulSoup(res.text, "html.parser")
         except requests.exceptions.RequestException as e:
             print(e)
@@ -34,7 +41,30 @@ class Librarian:
                 continue
         return categories
 
+    def __get_books(self, node: BeautifulSoup):
+        all_books = []
+        sections = node.find_all("div", class_=self.__constants["CAROUSEL_CLASS"])
+        for section in sections:
+            books = section.find_all("div", class_=self.__constants["CAROUSEL_ITEM_CLASS"])
+            for node in books:
+                all_books.append(Book(node))
+        return all_books
+
+    def __save_to_json(self, data):
+        with open("data/data.json", "w", encoding="utf-8") as f:
+            json.dump(data, f)
+
+    def __save_to_csv(self, data):
+        with open("data/data.csv", "w", encoding="utf-8", newline="") as f:
+            w = csv.writer(f)
+            w.writerow([key.capitalize() for key in Book.keys()])
+            for item in data:
+                w.writerow(item.values())
+
     def run(self):
         dom = self.__get_home_page_content()
-        categories = self.__get_categories(dom)
-        print(categories)
+        books = self.__get_books(dom)
+        self.__save_to_json([book.to_dict() for book in books])
+        self.__save_to_csv([book.to_dict() for book in books])
+        parser = JSON()
+        print(json)
